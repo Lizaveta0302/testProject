bf74c97493a91dd65339fa08e5c74600fa23601e
liza <liza.shpinkovaa@mail.ru>
2020-03-21 18:10:04 +0300

    commit on jpaHibernate, controller

--- src/main/java/com/example/testProject/HelloController.java
+++ src/main/java/com/example/testProject/HelloController.java
@@ -1,12 +1,19 @@
 package com.example.testProject;
 
+import com.example.testProject.entity.Message;
+import com.example.testProject.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
+
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/hello")
     public String hello(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
@@ -14,16 +21,24 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello";
     }
-    @GetMapping("/hello2")
-    public String hello2(@RequestParam(name = "name", required = false, defaultValue = "World2") String name,
-                        Model model) {
-        model.addAttribute("name", name);
-        return "hello2";
+
+    @GetMapping
+    public String main(Model model) {
+        Iterable<Message> messages = messageRepo.findAll();
+        model.addAttribute("message", messages);
+        return "hello";
     }
-    @GetMapping("/hello3")
-    public String hello3(@RequestParam(name = "name", required = false, defaultValue = "World3") String name,
-                        Model model) {
-        model.addAttribute("name", name);
-        return "hello3";
+
+    @PostMapping
+    public String addMessage(@RequestParam String text,
+                             @RequestParam String tag,
+                             Model model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.addAttribute("message", messages);
+
+        return "hello";
     }
+
 }
