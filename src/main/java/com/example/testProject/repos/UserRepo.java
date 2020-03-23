416b2d5ba476dc9708a7ff360e818a390f15be83
liza <liza.shpinkovaa@mail.ru>
2020-03-24 01:07:37 +0300

    add Spring Boot Mail and activation account and fix button Sign Out

--- src/main/java/com/example/testProject/repos/UserRepo.java
+++ src/main/java/com/example/testProject/repos/UserRepo.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UserRepo extends JpaRepository<User, Long> {
-User findByUsername(String username);
+    User findByUsername(String username);
+
+    User findByActivationCode(String code);
 }
