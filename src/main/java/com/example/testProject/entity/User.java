0dba14d350fc389782b4e95908343ba3089dedcb
liza <liza.shpinkovaa@mail.ru>
2020-03-25 15:11:09 +0300

    fix Confirmation password

--- src/main/java/com/example/testProject/entity/User.java
+++ src/main/java/com/example/testProject/entity/User.java
@@ -20,9 +20,6 @@ public class User implements UserDetails {
     private String username;
     @NotBlank(message = "Password cannot be empty")
     private String password;
-    @Transient
-    @NotBlank(message = "Password confirmation cannot be empty")
-    private String password2;
     @Email(message = "Email is not correct")
     @NotBlank(message = "Email cannot be empty")
     private String email;
@@ -96,14 +93,6 @@ public class User implements UserDetails {
 
     public void setActivationCode(String activationCode) {
         this.activationCode = activationCode;
-    }
-
-    public String getPassword2() {
-        return password2;
-    }
-
-    public void setPassword2(String password2) {
-        this.password2 = password2;
     }
 
     @Override
