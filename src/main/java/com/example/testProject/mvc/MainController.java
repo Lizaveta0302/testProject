1709c8300b104c6592b47967c7a3c8311a9380aa
liza <liza.shpinkovaa@mail.ru>
2020-04-05 16:33:17 +0300

    display images from db

--- src/main/java/com/example/testProject/mvc/MainController.java
+++ src/main/java/com/example/testProject/mvc/MainController.java
@@ -7,20 +7,25 @@ import com.example.testProject.repos.FileRepository;
 import com.example.testProject.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StreamUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
+import java.sql.Blob;
+import java.sql.SQLException;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 @Controller
@@ -71,6 +76,7 @@ public class MainController {
 
                 FileModel fileModel = new FileModel(file.getOriginalFilename(), file.getContentType(), file.getBytes());
                 fileRepository.save(fileModel);
+
                 message.setAuthor(user);
                 message.setFile(fileModel);
                 //fileModel.setMessage(message);
@@ -84,4 +90,35 @@ public class MainController {
 
         return "redirect:/main";
     }
+
+    @ResponseBody
+    @GetMapping("/api/file/{id}")
+    public ResponseEntity<byte[]> getFile(@PathVariable Long id) {
+        Optional<FileModel> fileOptional = fileRepository.findById(id);
+        byte[] imageBytes = null;
+        if (fileOptional.isPresent()) {
+            //FileModel file = fileOptional.get();
+            imageBytes = fileOptional.get().getPic();
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG).body(imageBytes);
+                    /*.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
+                    .body(file.getPic());*/
+        }
+        return ResponseEntity.status(404).body(null);
+    }
+
+   /* @GetMapping(value = "img/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public void fromDatabaseAsHttpServResp(@PathVariable("id") Long id, HttpServletResponse response)
+            throws SQLException, IOException {
+
+        Optional<FileModel> fileModelOptional = fileRepository.findById(id);
+
+        if (fileModelOptional.isPresent()) {
+
+            byte[] image = fileModelOptional.get().getPic();
+
+            StreamUtils.copy(image, response.getOutputStream());
+        }
+    }*/
+
 }
